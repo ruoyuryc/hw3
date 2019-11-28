d3b4ea2c7fbd290063bc12238f4311d9fa989041
ruoyuryc <56672302+ruoyuryc@users.noreply.github.com>
2019-11-28 22:06:47 +0800

    Add files via upload

---
+++ retina/utils.py
@@ -0,0 +1,259 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""A set of functions that are used for visualization.
+These functions often receive an image, perform some visualization on the image.
+The functions do not return a value, instead they modify the image itself.
+"""
+import collections
+import numpy as np
+import PIL.Image as Image
+import PIL.ImageColor as ImageColor
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
+
+
+_TITLE_LEFT_MARGIN = 10
+_TITLE_TOP_MARGIN = 10
+STANDARD_COLORS = [
+        'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
+        'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
+        'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
+        'DarkCyan', 'DarkGoldenRod', 'DarkGrey', 'DarkKhaki', 'DarkOrange',
+        'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkTurquoise', 'DarkViolet',
+        'DeepPink', 'DeepSkyBlue', 'DodgerBlue', 'FireBrick', 'FloralWhite',
+        'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
+        'Salmon', 'Tan', 'HoneyDew', 'HotPink', 'IndianRed', 'Ivory', 'Khaki',
+        'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue',
+        'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey',
+        'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
+        'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime',
+        'LimeGreen', 'Linen', 'Magenta', 'MediumAquaMarine', 'MediumOrchid',
+        'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen',
+        'MediumTurquoise', 'MediumVioletRed', 'MintCream', 'MistyRose', 'Moccasin',
+        'NavajoWhite', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
+        'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed',
+        'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
+        'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Green', 'SandyBrown',
+        'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
+        'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'GreenYellow',
+        'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White',
+        'WhiteSmoke', 'Yellow', 'YellowGreen'
+]
+
+
+def draw_bounding_box_on_image_array(image,
+                                                                         ymin,
+                                                                         xmin,
+                                                                         ymax,
+                                                                         xmax,
+                                                                         color='red',
+                                                                         thickness=4,
+                                                                         display_str_list=(),
+                                                                         use_normalized_coordinates=True):
+    """Adds a bounding box to an image (numpy array).
+    Bounding box coordinates can be specified in either absolute (pixel) or
+    normalized coordinates by setting the use_normalized_coordinates argument.
+    Args:
+        image: a numpy array with shape [height, width, 3].
+        ymin: ymin of bounding box.
+        xmin: xmin of bounding box.
+        ymax: ymax of bounding box.
+        xmax: xmax of bounding box.
+        color: color to draw bounding box. Default is red.
+        thickness: line thickness. Default value is 4.
+        display_str_list: list of strings to display in box
+                                            (each to be shown on its own line).
+        use_normalized_coordinates: If True (default), treat coordinates
+            ymin, xmin, ymax, xmax as relative to the image.    Otherwise treat
+            coordinates as absolute.
+    """
+    image_pil = Image.fromarray(np.uint8(image)).convert('RGB')
+    draw_bounding_box_on_image(image_pil, ymin, xmin, ymax, xmax, color,
+                                                         thickness, display_str_list,
+                                                         use_normalized_coordinates)
+    np.copyto(image, np.array(image_pil))
+
+
+def draw_bounding_box_on_image(image,
+                                                             ymin,
+                                                             xmin,
+                                                             ymax,
+                                                             xmax,
+                                                             color='red',
+                                                             thickness=4,
+                                                             display_str_list=(),
+                                                             use_normalized_coordinates=True):
+    """Adds a bounding box to an image.
+    Bounding box coordinates can be specified in either absolute (pixel) or
+    normalized coordinates by setting the use_normalized_coordinates argument.
+    Each string in display_str_list is displayed on a separate line above the
+    bounding box in black text on a rectangle filled with the input 'color'.
+    If the top of the bounding box extends to the edge of the image, the strings
+    are displayed below the bounding box.
+    Args:
+        image: a PIL.Image object.
+        ymin: ymin of bounding box.
+        xmin: xmin of bounding box.
+        ymax: ymax of bounding box.
+        xmax: xmax of bounding box.
+        color: color to draw bounding box. Default is red.
+        thickness: line thickness. Default value is 4.
+        display_str_list: list of strings to display in box
+                                            (each to be shown on its own line).
+        use_normalized_coordinates: If True (default), treat coordinates
+            ymin, xmin, ymax, xmax as relative to the image.    Otherwise treat
+            coordinates as absolute.
+    """
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+    if use_normalized_coordinates:
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                                                    ymin * im_height, ymax * im_height)
+    else:
+        (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
+    draw.line([(left, top), (left, bottom), (right, bottom),
+                         (right, top), (left, top)], width=thickness, fill=color)
+    try:
+        font = ImageFont.truetype('arial.ttf', 24)
+    except IOError:
+        font = ImageFont.load_default()
+
+    # If the total height of the display strings added to the top of the bounding
+    # box exceeds the top of the image, stack the strings below the bounding box
+    # instead of above.
+    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+    # Each display_str has a top and bottom margin of 0.05x.
+    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+    if top > total_display_str_height:
+        text_bottom = top
+    else:
+        text_bottom = bottom + total_display_str_height
+    # Reverse list and print from bottom to top.
+    for display_str in display_str_list[::-1]:
+        text_width, text_height = font.getsize(display_str)
+        margin = np.ceil(0.05 * text_height)
+        draw.rectangle(
+                [(left, text_bottom - text_height - 2 * margin), (left + text_width,
+                                                                                                                    text_bottom)],
+                fill=color)
+        draw.text(
+                (left + margin, text_bottom - text_height - margin),
+                display_str,
+                fill='black',
+                font=font)
+        text_bottom -= text_height - 2 * margin
+
+
+def draw_mask_on_image_array(image, mask, color='red', alpha=0.4):
+   
+    if image.dtype != np.uint8:
+        raise ValueError('`image` not of type np.uint8')
+    if mask.dtype != np.uint8:
+        raise ValueError('`mask` not of type np.uint8')
+    if np.any(np.logical_and(mask != 1, mask != 0)):
+        raise ValueError('`mask` elements should be in [0, 1]')
+    if image.shape[:2] != mask.shape:
+        raise ValueError('The image has spatial dimensions %s but the mask has '
+                                         'dimensions %s' % (image.shape[:2], mask.shape))
+    rgb = ImageColor.getrgb(color)
+    pil_image = Image.fromarray(image)
+
+    solid_color = np.expand_dims(
+            np.ones_like(mask), axis=2) * np.reshape(list(rgb), [1, 1, 3])
+    pil_solid_color = Image.fromarray(np.uint8(solid_color)).convert('RGBA')
+    pil_mask = Image.fromarray(np.uint8(255.0*alpha*mask)).convert('L')
+    pil_image = Image.composite(pil_solid_color, pil_image, pil_mask)
+    np.copyto(image, np.array(pil_image.convert('RGB')))
+
+
+def visualize_boxes_and_labels_on_image_array(    
+        image,
+        boxes,
+        classes,
+        scores,
+        data,
+        category_index,
+        instance_masks=None,
+        instance_boundaries=None,
+        use_normalized_coordinates=False,
+        max_boxes_to_draw=20,
+        min_score_thresh=.35,
+        agnostic_mode=False,
+        line_thickness=4,
+        groundtruth_box_visualization_color='black',
+        skip_scores=False,
+        skip_labels=False):
+   
+    # Create a display string (and color) for every box location, group any boxes
+    # that correspond to the same location.
+    box_to_display_str_map = collections.defaultdict(list)
+    box_to_color_map = collections.defaultdict(str)
+    box_to_instance_masks_map = {}
+    box_to_instance_boundaries_map = {}
+    if not max_boxes_to_draw:
+        max_boxes_to_draw = boxes.shape[0]
+    for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+        if scores is None or scores[i] > min_score_thresh:
+            box = tuple(boxes[i].tolist())
+            if instance_masks is not None:
+                box_to_instance_masks_map[box] = instance_masks[i]
+            if instance_boundaries is not None:
+                box_to_instance_boundaries_map[box] = instance_boundaries[i]
+            if scores is None:
+                box_to_color_map[box] = groundtruth_box_visualization_color
+            else:
+                display_str = ''
+                if not skip_labels:
+                    if not agnostic_mode:
+                        if classes[i] in category_index.keys():
+                            class_name = category_index[classes[i]]['name']
+                        else:
+                            class_name = 'N/A'
+                        display_str = str(class_name)
+                        #print(class_name)
+                        data['label'].append(int(class_name))
+                        
+                if not skip_scores:
+                    if not display_str:
+                        display_str = '{}%'.format(int(100*scores[i]))
+                    else:
+                        display_str = '{}: {}%'.format(display_str, int(100*scores[i]))
+                box_to_display_str_map[box].append(display_str)
+                #print(scores[i])
+                data['score'].append(float(scores[i]))
+                if agnostic_mode:
+                    box_to_color_map[box] = 'DarkOrange'
+                else:
+                    box_to_color_map[box] = STANDARD_COLORS[
+                            classes[i] % len(STANDARD_COLORS)]
+    
+    # Draw all boxes onto image.
+    for box, color in box_to_color_map.items():
+        ymin, xmin, ymax, xmax = box
+        data['bbox'].append([ymin, xmin, ymax, xmax])
+
+    return data
+
+
+# from yolo.utils.visualization_utils import visualize_boxes_and_labels_on_image_array
+def visualize_boxes(image, boxes, labels, probs, data, class_labels):
+    category_index = {}
+    for id_, label_name in enumerate(class_labels):
+        category_index[id_] = {"name": label_name}
+    
+    boxes = np.array([np.array([b[1],b[0],b[3],b[2]]) for b in boxes])
+    visualize_boxes_and_labels_on_image_array(image, boxes, labels, probs, data, category_index)
